fd93dc5d40cbaa5f5cd8b4fcc1d62c8727d33583
Victor Augusto <victor.augusto3159@outlook.com.br>
2021-03-13 19:08:34 -0300

    movidos arquivos para dentro de terraform/

--- up.sh
+++ up.sh
@@ -1,5 +1,14 @@
 #! /usr/bin/env bash
-echo "Starting terraform apply..."
-terraform apply \
-    --state=states/terrform.tfstate \
-    --state-out=states/terraform.tfstate
+apply() {
+    echo "Executing terraform init"
+    terraform init terraform/
+
+    echo "Starting terraform apply..."
+    terraform apply \
+        -state=terraform/states/terraform.tfstate \
+        -state-out=terraform/states/terraform.tfstate \
+        -backup=terraform/states/terraform.tfstate.backup \
+        terraform/
+}
+
+apply
