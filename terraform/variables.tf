3d08da92c8a0cbc8098dab51fe65c2d6b96fe6c7
Victor Augusto <victor.augusto3159@outlook.com.br>
2021-03-13 21:00:09 -0300

    renomeado clusters e configs

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,3 +11,15 @@ variable "worker_type" {
   default     = [ "t2.micro" ]
   description = "instance type for workers"
 }
+
+variable "credentials_path" {
+  type = string
+  description = "Credentials path"
+  default = "~/.aws/credentials"
+}
+
+variable "profile" {
+  type = string
+  description = "(optional) describe your variable"
+  default = "default"
+}
