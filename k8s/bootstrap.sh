8ce49a740cb716e27b62d5519ec18ec5ff6c76a7
Victor Augusto <victor.silva452@outlook.com>
2021-05-23 23:35:11 -0300

    :hammer: modificate bootstrap.sh

--- k8s/bootstrap.sh
+++ k8s/bootstrap.sh
@@ -1,11 +1,47 @@
 #! /bin/usr/env bash
+set -ex
 
-kubectl apply -f namespaces.yaml $*
+ACTION=${1:-"apply"}
 
-# Creating knative environment
-kubectl apply -f knative/serving-crds.yaml $*
-kubectl apply -f knative/serving-core.yaml $*
+do_namespaces() {
+    kubectl ${ACTION} -f namespaces.yaml $*
+}
 
-#Installing Istio
-kubectl apply -f knative/istio/pilot.yaml $*
-kubectl apply -f knative/istio/control-plane.yaml $*
+do_knative() {
+    kubectl ${ACTION} -f knative/serving-crds.yaml $*
+    kubectl ${ACTION} -f knative/serving-core.yaml $*
+}
+
+do_istio(){
+    kubectl ${ACTION} -f knative/istio/istio.yaml $* || true
+    kubectl ${ACTION} -f knative/istio/net-istio.yaml $* || true
+    kubectl ${ACTION} -f knative/istio/istio.yaml $* 
+}
+
+do_databases(){
+    kubectl ${ACTION} -f databases/secrets.yaml $*
+    kubectl ${ACTION} -f databases/config-map.yaml $*
+    kubectl ${ACTION} -f databases/mysql.yaml $*
+    kubectl ${ACTION} -f databases/redis.yaml $*
+}
+
+case $@ in
+    namespaces)
+        do_namespaces
+    ;;
+    knative)
+        do_knative
+    ;;
+    istio)
+        do_istio
+    ;;
+    databases|database)
+        do_databases
+    ;;
+    *)
+        do_namespaces
+        do_knative
+        do_istio
+        do_databases
+    ;;
+esac
